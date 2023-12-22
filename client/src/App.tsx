5b5015407b9594a5717410a10413cc906e5a7c66
Vinay Maurya <vinaymaurya105@gmail.com>
2023-12-22 23:52:01 +0530

    wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { Box } from "@mui/material";
 import React from "react";
-import Login from "./components/Login";
 import Home from "./components/Home";
 
 function App() {
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,21 +1,38 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import Login from "./Login";
+import { url } from "inspector";
+
+const imgUrl =
+  "https://img.freepik.com/free-vector/emoji-framed-background_53876-89766.jpg?size=626&ext=jpg&ga=GA1.1.933889225.1703267740&semt=ais";
 
 function Home() {
   return (
     <Box
       display="flex"
-      position="absolute"
+      // position="absolute"
       flexDirection="column"
-      left="60%"
-      style={{ translate: "-50% 0%" }}
+      // left="50%"
+      // top="50px"
       width="100%"
+      // height="100%"
+      flex={1}
+      style={{
+        backgroundImage: `url(${imgUrl})`,
+        backgroundRepeat: "no-repeat",
+      }}
     >
-      <Box>
+      <Box
+        width="35%"
+        border="1px solid #B0A695"
+        height={40}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
         <Typography>Talk-A-Tive</Typography>
       </Box>
-      <Box width="30%">
+      <Box width="35%" height="100%">
         <Login />
       </Box>
     </Box>
