27d2fa51dd6db848139769ec52e10752ea0fa993
Vinay Maurya <vinaymaurya105@gmail.com>
2023-12-25 23:28:35 +0530

    user model is defined

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,11 @@
 const express = require("express");
+
 const app = express();
 
 const PORT = 4002;
+
+app.get("/", (req: any, res: any) => {
+  res.send("Server is running");
+});
 
 app.listen(PORT, () => console.log(`app was listening ${PORT}`));
