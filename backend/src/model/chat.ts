0677048979dd90a513c4ff77df05178aad3db424
Vinay Maurya <vinaymaurya105@gmail.com>
2023-12-27 22:56:45 +0530

    message model created

--- backend/src/model/chat.ts
+++ backend/src/model/chat.ts
@@ -2,13 +2,16 @@ import mongoose, { model, Schema } from "mongoose";
 
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const ChatSchema = new Schema({
-  chatName: { type: String, trim: true },
-  isGroupChat: { type: Boolean, default: false },
-  users: [ObjectId],
-  latestMessage: ObjectId,
-  groupAdmin: ObjectId,
-});
+const ChatSchema = new Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [ObjectId],
+    latestMessage: ObjectId,
+    groupAdmin: ObjectId,
+  },
+  { timestamps: true }
+);
 
 const chatModel = model("chat", ChatSchema);
 
