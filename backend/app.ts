20f1d14eac0ad22fab53f1c51bd8e8ef21c951c5
Vinay Maurya <vinaymaurya105@gmail.com>
2023-12-31 22:33:25 +0530

    db connection done

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
 const PORT = 4002;
 
-app.get("/", (req: any, res: any) => {
-  res.send("Server is running");
-});
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017", { dbName: "chatapp" });
 
-app.listen(PORT, () => console.log(`app was listening on  ${PORT}`));
+  app.get("/", (req: any, res: any) => {
+    res.send("Server is running");
+  });
+
+  app.listen(PORT, () => console.log(`app was listening on  ${PORT}`));
+}
+
+main();
