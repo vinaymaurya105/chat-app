6825cf4c379ab87e6c8e38661b2db80702e10776
Vinay Maurya <vinaymaurya105@gmail.com>
2024-01-09 23:15:52 +0530

    Home page layout done

--- client/src/components/HomePageLayout.tsx
+++ client/src/components/HomePageLayout.tsx
@@ -3,53 +3,43 @@ import { makeStyles } from "@mui/styles";
 import { Link } from "react-router-dom";
 
 const useStyle = makeStyles({
-  container: {
+  continer: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 2,
+    flexDirection: "column",
+    gap: 16,
+    paddingTop: 20,
+  },
+  wrapper: {
+    display: "flex",
+    justifyContent: "center",
+    borderRadius: "6px",
     maxWidth: 500,
+    width: "100%",
+    background: "#fff",
+    flexDirection: "column",
   },
 });
 
 function HomePageLayout(props: any) {
   const { label, children } = props;
+
+  const classes = useStyle();
+
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      gap={2}
-      p={2}
-    >
+    <Box className={classes.continer}>
       <Box
+        className={classes.wrapper}
         height={40}
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        bgcolor="#fff"
-        borderRadius={2}
         p="2px 16px"
-        maxWidth={500}
-        width="100%"
+        alignItems="center"
       >
         <Typography variant="h5" color="grey">
           Talk-A-Tive
         </Typography>
       </Box>
-      <Box
-        maxWidth={500}
-        width="100%"
-        height="100%"
-        bgcolor="#fff"
-        borderRadius={2}
-        p={2}
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        gap={1.5}
-      >
+      <Box p={2} gap={1.5} className={classes.wrapper}>
         <Typography variant="h6">{label}</Typography>
         {children}
       </Box>
