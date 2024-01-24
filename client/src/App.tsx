f3cd3975ecd340a60cc450ba0fca7f06c50f34a7
Vinay Maurya <vinaymaurya105@gmail.com>
2024-01-24 23:01:06 +0530

    chat ui added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Chat from "./components/Chat";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/login" Component={Login} />
           <Route path="/signup" Component={Signup} />
-          <Route path="/chat" />
+          <Route path="/chat" Component={Chat} />
           <Route path="*" element={<Navigate to="/login" replace={true} />} />
         </Routes>
       </BrowserRouter>
