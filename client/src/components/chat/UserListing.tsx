45a300c9819dc2acef512e922b0752563868df11
Vinay Maurya <vinaymaurya105@gmail.com>
2024-02-07 23:42:28 +0530

    wip

--- client/src/components/chat/UserListing.tsx
+++ client/src/components/chat/UserListing.tsx
@@ -1,11 +1,20 @@
-import { Box, Button, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
 import Header from "./Header";
 import { makeStyles } from "@mui/styles";
+import SearchIcon from "@mui/icons-material/Search";
+import { ChangeEvent, useState } from "react";
+import { Close } from "@mui/icons-material";
 
 const useStyle = makeStyles(() => ({
   input: {
     "& .MuiFilledInput-root": {
-      padding: "5px 8px",
+      padding: "5px 15px",
       height: "35px",
       borderRadius: 8,
     },
@@ -18,6 +27,18 @@ const useStyle = makeStyles(() => ({
 
 function userListing() {
   const classes = useStyle();
+
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearch(value);
+  };
+
+  const handleCancel = () => {
+    setSearch("");
+  };
+
   return (
     <Box bgcolor="#fff" width={330} height="100%">
       <Header>
@@ -34,7 +55,26 @@ function userListing() {
           fullWidth
           className={classes.input}
           variant="filled"
-          sx={{ border: "none" }}
+          placeholder="Searh or start new chat"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start" style={{ marginTop: 0 }}>
+                <IconButton size="small">
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+            disableUnderline: true,
+            endAdornment: search.length > 0 && (
+              <InputAdornment position="end">
+                <IconButton size="small" onClick={handleCancel}>
+                  <Close style={{ height: 18, width: 18 }} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          value={search}
+          onChange={handleSearch}
         />
       </Box>
     </Box>
