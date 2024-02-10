b1279fe33148e78f683917b09fb000459dbce9ba
Vinay Maurya <vinaymaurya105@gmail.com>
2024-02-10 23:22:10 +0530

    profile wip

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,14 +1,82 @@
-import { Avatar, Box } from "@mui/material";
+import { ModeEdit } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  ClickAwayListener,
+  Grow,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { useRef, useState } from "react";
 
 type ProfileType = { size?: number; editable?: boolean };
 
+const useStyles = makeStyles(() => ({
+  editIconBox: {
+    position: "absolute",
+    display: "none",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "50%",
+    background: "rgba(0,0,0, 0.6)",
+    color: "#fff",
+    height: "100%",
+    width: "100%",
+  },
+
+  container: {
+    position: "relative",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+    borderRadius: "50%",
+    "&:hover $editIconBox": {
+      display: "flex",
+    },
+  },
+}));
+
 function Profile(props: ProfileType) {
   const { size = 40 } = props;
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const anchoreRef = useRef<HTMLAnchorElement | null>(null);
   return (
     <Box>
-      <Avatar style={{ height: size, width: size }} />
-      <input type="file" />
-      <Box height={size} width={size} bgcolor="red" borderRadius={50} />
+      <Box className={classes.container} height={size} width={size}>
+        <Avatar style={{ height: size, width: size }} />
+        <Box className={classes.editIconBox} onClick={() => setOpen(true)}>
+          <ModeEdit style={{ height: size / 2 }} />
+        </Box>
+        {/* <input type="file" /> */}
+        <Popper
+          open={open}
+          anchorEl={anchoreRef.current}
+          transition
+          style={{ zIndex: 1500 }}
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center-top" : "left-top",
+              }}
+            >
+              <Paper>
+                {/* <ClickAwayListener onClickAway={}> */}
+                <MenuList>
+                  <MenuItem>UploadImage</MenuItem>
+                </MenuList>
+                {/* </ClickAwayListener> */}
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </Box>
     </Box>
   );
 }
