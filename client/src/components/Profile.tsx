0a4c38533761b940a6b0038fc49d0897cf4e4e41
Vinay Maurya <vinaymaurya105@gmail.com>
2024-02-11 20:24:18 +0530

    profile component created

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -10,9 +10,9 @@ import {
   Popper,
 } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import { useRef, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 
-type ProfileType = { size?: number; editable?: boolean };
+type ProfileType = { size?: number; editable?: boolean; icon?: string };
 
 const useStyles = makeStyles(() => ({
   editIconBox: {
@@ -25,6 +25,7 @@ const useStyles = makeStyles(() => ({
     color: "#fff",
     height: "100%",
     width: "100%",
+    cursor: "pointer",
   },
 
   container: {
@@ -39,39 +40,88 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+const imgValidation = "image/png,image/gif,image/jpg,image/jpeg";
+
 function Profile(props: ProfileType) {
-  const { size = 40 } = props;
+  const { size = 40, icon = "" } = props;
   const classes = useStyles();
   const [open, setOpen] = useState(false);
   const anchoreRef = useRef<HTMLAnchorElement | null>(null);
+
+  const [img, setImage] = useState<string | undefined>(icon);
+
+  const handleClickAway = () => {
+    setOpen(false);
+  };
+
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    handleClickAway();
+    const files = event.target.files || [];
+    const file = files[0];
+    if (!file) return;
+    const fileReader = new FileReader();
+    fileReader.onloadend = (e: ProgressEvent<FileReader>) => {
+      const result = e.target?.result as string;
+      setImage(result);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
+  const handleRemoveImage = () => {
+    handleClickAway();
+    if (!img) return;
+    setImage("");
+  };
   return (
     <Box>
-      <Box className={classes.container} height={size} width={size}>
-        <Avatar style={{ height: size, width: size }} />
-        <Box className={classes.editIconBox} onClick={() => setOpen(true)}>
+      <Box
+        ref={anchoreRef}
+        className={classes.container}
+        height={size}
+        width={size}
+      >
+        <Avatar src={img} style={{ height: size, width: size }} />
+        <Box
+          className={classes.editIconBox}
+          onClick={() => setOpen((prev) => !prev)}
+        >
           <ModeEdit style={{ height: size / 2 }} />
         </Box>
-        {/* <input type="file" /> */}
         <Popper
           open={open}
           anchorEl={anchoreRef.current}
           transition
-          style={{ zIndex: 1500 }}
+          style={{ zIndex: 1300 }}
         >
           {({ TransitionProps, placement }) => (
             <Grow
               {...TransitionProps}
               style={{
                 transformOrigin:
-                  placement === "bottom" ? "center-top" : "left-top",
+                  placement === "bottom" ? "center-bottom" : "left-top",
               }}
             >
               <Paper>
-                {/* <ClickAwayListener onClickAway={}> */}
-                <MenuList>
-                  <MenuItem>UploadImage</MenuItem>
-                </MenuList>
-                {/* </ClickAwayListener> */}
+                <ClickAwayListener onClickAway={handleClickAway}>
+                  <MenuList id="menu-list-grow">
+                    <MenuItem component="label" style={{ fontSize: 12 }}>
+                      Upload image
+                      <input
+                        type="file"
+                        hidden
+                        accept={imgValidation}
+                        onChange={handleInput}
+                      />
+                    </MenuItem>
+                    <MenuItem
+                      style={{ fontSize: 12 }}
+                      disabled={!img}
+                      onClick={handleRemoveImage}
+                    >
+                      Remove image
+                    </MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
               </Paper>
             </Grow>
           )}
