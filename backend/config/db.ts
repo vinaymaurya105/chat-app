77f3f65ee4009c2aa60012bd9572a14254712f81
Vinay Maurya <vinaymaurya105@gmail.com>
2024-03-05 23:40:54 +0530

    GroupChat api fetch

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -5,7 +5,6 @@ async function connectDb() {
     await mongoose.connect(process.env.DB_URL as string, {
       dbName: process.env.DB_NAME,
     });
-    console.log("Connection successful");
   } catch (err) {
     console.log(`Error ${err}`);
     process.exit();
--- client/src/components/chat/GroupChat.tsx
+++ client/src/components/chat/GroupChat.tsx
@@ -12,46 +12,60 @@ import React, { useEffect, useState } from "react";
 import { LIST_USERS } from "../../utils/constants/api";
 import { Close } from "@mui/icons-material";
 import axios from "axios";
+import Loader from "../Loader";
+import { getReqHeaders } from "../../utils/Helper";
 
 function GroupChat(props: any) {
   const { handleGroupChat } = props;
   console.log("hell");
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const config = { method: "GET", url: LIST_USERS };
-    axios(config).then((res) => {
-      const { success, message, result } = res.data;
-    });
-  });
+    const config = { method: "GET", url: LIST_USERS, headers: getReqHeaders() };
+    setLoading(true);
+    axios(config)
+      .then((res) => {
+        const { success, message, result } = res.data;
+        if (!success) throw new Error(message);
+        setUsers(result);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
   return (
     <Dialog open onClose={handleGroupChat} maxWidth="lg">
-      <Box width={550}>
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          p="9px 20px"
-          height={50}
-          boxSizing="border-box"
-          alignItems="center"
-        >
-          <DialogTitle style={{ padding: 0 }}>
-            <Typography variant="h6">Add group members</Typography>
-          </DialogTitle>
-          <IconButton onClick={handleGroupChat}>
-            <Close />
-          </IconButton>
-        </Box>
-
-        <DialogContent dividers>
-          <Box height="100%" flex={1}>
-            Hell
+      <Loader loading={loading}>
+        <Box width={550}>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            p="9px 20px"
+            height={50}
+            boxSizing="border-box"
+            alignItems="center"
+          >
+            <DialogTitle style={{ padding: 0 }}>
+              <Typography variant="h6">Add group members</Typography>
+            </DialogTitle>
+            <IconButton onClick={handleGroupChat}>
+              <Close />
+            </IconButton>
           </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button>Next</Button>
-        </DialogActions>
-      </Box>
+
+          <DialogContent dividers>
+            <Box height="100%" flex={1}>
+              Hell
+            </Box>
+          </DialogContent>
+          <DialogActions>
+            <Button>Next</Button>
+          </DialogActions>
+        </Box>
+      </Loader>
     </Dialog>
   );
 }
